21707261dcf09028acc4af0653f37e8b658cf5f6
Neil Pankey <npankey@gmail.com>
2015-03-26 22:41:21 -0700

    Flush out Editor

--- Source/Editor.swift
+++ Source/Editor.swift
@@ -7,6 +7,12 @@ public struct Editor {
     public init() {
     }
 
+    /// Construct an `Editor` from `str` placing cursor at home
+    public init(str: String) {
+        tail = str
+    }
+
+
     /// The prefix of the string
     public private(set) var head = ""
 
@@ -18,6 +24,34 @@ public struct Editor {
         return head + tail
     }
 
+
+    /// Move backward one character if `head` is non-empty
+    public mutating func left() {
+        if let char = dropLast(&head) {
+            tail.insert(char, atIndex: tail.startIndex)
+        }
+    }
+
+    /// Move forward one character if `tail` is non-empty
+    public mutating func right() {
+        if let char = dropFirst(&tail) {
+            head.append(char)
+        }
+    }
+
+    /// Moves to the start of `text`
+    public mutating func home() {
+        tail = head + tail
+        head = ""
+    }
+
+    /// Moves to the end of `text`
+    public mutating func end() {
+        head.extend(tail)
+        tail = ""
+    }
+
+
     /// Insert a new character at the end of `head`
     public mutating func insert(char: Character) {
         head.append(char)
@@ -26,5 +60,41 @@ public struct Editor {
     /// Insert a new string at the end of `head`
     public mutating func insert(str: String) {
         head.extend(str)
+    }
+    /// Delete the last character of `head` if non-empty
+    public mutating func backspace() {
+        dropLast(&head)
+    }
+
+    /// Delete the first character of `tail` if non-empty
+    public mutating func delete() {
+        dropFirst(&tail)
+    }
+
+    /// Deletes all text in `Editor`
+    public mutating func clear() {
+        head = ""
+        tail = ""
+    }
+
+    /// Deletes all text in `head`
+    public mutating func clearHome() {
+        head = ""
+    }
+
+    /// Deletes all text in `tail`
+    public mutating func clearEnd() {
+        tail = ""
+    }
+
+
+    /// Remove the first character from `str`
+    private func dropFirst(inout str: String) -> Character? {
+        return !isEmpty(str) ? str.removeAtIndex(str.startIndex) : nil
+    }
+
+    /// Remove the last character from `str`
+    private func dropLast(inout str: String) -> Character? {
+        return !isEmpty(str) ? str.removeAtIndex(str.endIndex.predecessor()) : nil
     }
 }
