4aaaa0bdd0581a487b772547687f64606b31014c
Neil Pankey <npankey@gmail.com>
2015-03-26 21:38:38 -0700

    Xcode project

---
+++ Lines/Lines.h
@@ -0,0 +1,8 @@
+//  Copyright (c) 2015 Neil Pankey. All rights reserved.
+
+/// Project version number for Lines.
+extern double LinesVersionNumber;
+
+/// Project version string for Lines.
+extern const unsigned char LinesVersionString[];
+
