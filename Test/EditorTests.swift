52fbd21ec4f9d8bc0095ca24a1544cbb7e8c9fa5
Neil Pankey <npankey@gmail.com>
2015-03-26 23:04:43 -0700

    Editor tests

--- Test/EditorTests.swift
+++ Test/EditorTests.swift
@@ -5,11 +5,9 @@ import Lines
 
 final class EditorTests: XCTestCase {
 
-    func testEmpty() {
-        var editor = Editor()
-        XCTAssertEqual(editor.head, "")
-        XCTAssertEqual(editor.tail, "")
-        XCTAssertEqual(editor.text, "")
+    func testConstruction() {
+        verify(Editor(), "", "")
+        verify(Editor(str: "asdf"), "", "asdf")
     }
 
     func testInsert() {
@@ -23,5 +21,81 @@ final class EditorTests: XCTestCase {
 
         editor.insert("cde")
         XCTAssertEqual(editor.text, "abcde")
+    }
+
+    func testMovement() {
+        var editor = Editor(str: "123")
+
+        editor.left()
+        verify(editor, "", "123")
+
+        editor.right()
+        verify(editor, "1", "23")
+
+        editor.end()
+        verify(editor, "123", "")
+
+        editor.right()
+        verify(editor, "123", "")
+
+        editor.left()
+        verify(editor, "12", "3")
+
+        editor.home()
+        verify(editor, "", "123")
+    }
+
+    func testRemoval() {
+        var editor = Editor(str: "qwerty")
+
+        editor.backspace()
+        verify(editor, "", "qwerty")
+
+        editor.right()
+        editor.backspace()
+        verify(editor, "", "werty")
+
+        editor.delete()
+        verify(editor, "", "erty")
+
+        editor.end()
+        editor.delete()
+        verify(editor, "erty", "")
+
+        editor.left()
+        editor.backspace()
+        verify(editor, "er", "y")
+
+        editor.left()
+        editor.delete()
+        verify(editor, "e", "y")
+    }
+
+    func testClear() {
+        var editor = Editor(str: "foobar")
+
+        editor.clearHome()
+        verify(editor, "", "foobar")
+
+        editor.end()
+        editor.clearEnd()
+        verify(editor, "foobar", "")
+
+        editor.left(); editor.left();
+        editor.clearEnd()
+        verify(editor, "foob", "")
+
+        editor.left(); editor.left();
+        editor.clearHome()
+        verify(editor, "", "ob")
+
+        editor.clear()
+        verify(editor, "", "")
+    }
+
+    private func verify(editor: Editor, _ head: String, _ tail: String, file: String = __FILE__, line: UInt = __LINE__) {
+        XCTAssertEqual(editor.head, head, "", file: file, line: line)
+        XCTAssertEqual(editor.tail, tail, "", file: file, line: line)
+        XCTAssertEqual(editor.text, head + tail, "", file: file, line: line)
     }
 }
