99eec19f0024a96449804b75d186f45a3eb1bbaa
Neil Pankey <npankey@gmail.com>
2015-03-27 20:25:33 -0700

    String extensions for Cursor

--- Source/Cursor.swift
+++ Source/Cursor.swift
@@ -37,3 +37,30 @@ public func ==(lhs: Cursor, rhs: Cursor) -> Bool {
 public func <(lhs: Cursor, rhs: Cursor) -> Bool {
     return lhs.index < rhs.index
 }
+
+extension String {
+    /// `Cursor` at the start of string
+    public var startCursor: Cursor {
+        return Cursor(string: self)
+    }
+
+    /// Lookup the character at `cursor`
+    public subscript(cursor: Cursor) -> Character {
+        return self[cursor.index]
+    }
+
+    /// Lookup the string in `range`
+    public subscript(range: Range<Cursor>) -> String {
+        return self[range.startIndex.index..<range.endIndex.index]
+    }
+
+    /// Is `cursor` at the end of the string
+    public func isEOF(cursor: Cursor) -> Bool {
+        return cursor.index == endIndex
+    }
+
+    /// Is `cursor` at the end of a line in string
+    public func isEOL(cursor: Cursor) -> Bool {
+        return isEOF(cursor) || self[cursor].isNewline
+    }
+}
